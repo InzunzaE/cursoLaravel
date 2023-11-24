46948924ed91caa38c312159e0de40c538a0bfb9
Eitan Inzunza <eitanmisael14@gmail.com>
2023-11-23 18:58:13 -0700

    23/11/23 zzz

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Alumno extends Model
 {
     use HasFactory;
+
+    protected $table = "alumnos";
+
+    protected $fillable = [
+        'nombre',
+        'edad',
+        'nc',
+        'sexo'
+    ];
 }
